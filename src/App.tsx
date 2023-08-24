d70cde69732c1b306e35ab44e29404f559cccff5
user <like@admin.com>
2023-08-24 17:38:16 +0800

    feat: route lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
-import Home from "@/pages/home/index";
-import Battle from "@/pages/battle/index";
+// import Home from "@/pages/home/index";
+// import Battle from "@/pages/battle/index";
+const Home = lazy(() => import('@/pages/home/index'));
+const Battle = lazy(() => import('@/pages/battle/index'));
 
 export default function App() {
     return (
         <BrowserRouter>
-            <div className='py-3'>
-                <Link to="/" className='mx-8'>Home</Link>
-                <Link to="/battle" className='mx-8'>Battle</Link>
-            </div>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/battle" element={<Battle />} />
-            </Routes>
+            <Suspense fallback={<></>}>
+                <div className='py-3'>
+                    <Link to="/" className='mx-8'>Home</Link>
+                    <Link to="/battle" className='mx-8'>Battle</Link>
+                </div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/battle" element={<Battle />} />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     )
 }
